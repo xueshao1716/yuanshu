6ddcb8220b4716e35d803d442c4e36a6e4542620
linxinyu520xue <linxinyu520xue@users.noreply.gitee.com>
2026-09-16 16:09:57 +0800

    chore(release): v2.43.0

--- frontend/dist/assets/railroad-peg-WCYAUIDC-DFUnTsU8.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-FOHPRMQF-DHwB1DNv.js";import{u as e}from"./mermaid-parser.core-DdhrLJRV.js";export{e as createRailroadPegServices};
---
+++ public/assets/swimlanesDiagram-VR7AAH4N-DluGu_4X.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-B6xuSHsQ.js";import"./chunk-DU6HZSFF-C-8jFoSs.js";import"./chunk-75Z2AOVW-BO21b1bx.js";import"./chunk-PWAF6VOD-x19LeT8n.js";import"./chunk-GMAD6QVW-aOtlEIf6.js";import"./chunk-P2QGCYS3-uyhbAweg.js";import"./chunk-4HAMMTFA-CHScGzUt.js";import"./chunk-GVQU2GXP-DrfXEvHy.js";import"./chunk-OSK3NFVY-DNCZVXHu.js";import"./chunk-F27PBJKO-CdrOR-EX.js";import"./chunk-XXDRQBXY-DqAYeljN.js";import"./chunk-POPQ4Y6H-2k4exqQK.js";import"./chunk-L3NEJ4N5-DL9hE8MK.js";import{r as t,t as n}from"./chunk-SHT3W25Y-inq5DrtI.js";import"./mermaid.core-BJbhbEwv.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
