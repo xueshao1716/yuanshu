6e7e2b7187254f7df298e4740e2de60bac1c54bb
linxinyu520xue <linxinyu520xue@users.noreply.gitee.com>
2026-09-23 18:40:56 +0800

    fix: include 2.115.12 client snapshot assets

---
+++ app/dist/assets/swimlanesDiagram-VR7AAH4N-BlT_tm3C.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-B6xuSHsQ.js";import"./chunk-DU6HZSFF-Dw5CI465.js";import"./chunk-75Z2AOVW-C1n-eKH_.js";import"./chunk-PWAF6VOD-BDUN-pwr.js";import"./chunk-GMAD6QVW-Dj5VlGJS.js";import"./chunk-P2QGCYS3-DPdVC0Od.js";import"./chunk-4HAMMTFA-BrDwP6VO.js";import"./chunk-GVQU2GXP-c8S7dlI5.js";import"./chunk-OSK3NFVY-Cy9_rezJ.js";import"./chunk-F27PBJKO-BTrBH9mn.js";import"./chunk-XXDRQBXY-DqAYeljN.js";import"./chunk-POPQ4Y6H-ZpvowWtY.js";import"./chunk-L3NEJ4N5-DMj4UQrs.js";import{r as t,t as n}from"./chunk-SHT3W25Y-IKTOX1PX.js";import"./mermaid.core-BsT2uBt8.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
